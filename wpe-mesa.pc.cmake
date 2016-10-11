5a84f2b9659653e3d33bc054baa7f0173883d18f
Claudio Saavedra <csaavedra@igalia.com>
2016-10-11 17:01:40 +0300

    Use a pkg-config instead of CMake to export the library
    Also use wpe's new pkg-config file instead of relying on cmake

---
+++ wpe-mesa.pc.cmake
@@ -0,0 +1,11 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=${prefix}
+includedir=${prefix}/include
+libdir=${exec_prefix}/lib
+
+Name: wpe-mesa
+Description: The mesa backend for the wpe library
+Version: @WPE_MESA_VERSION@
+Requires: wpe
+Cflags: -I${includedir}
+Libs: -L${libdir} -lWPE-mesa
